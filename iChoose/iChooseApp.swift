c9bae9d9f39a7a30ca9dde867a071a9784557359
Giovanni <giovannidv96@gmail.com>
2021-10-21 18:30:02 +0200

    Initial Commit

---
+++ iChoose/iChooseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChooseApp.swift
+//  iChoose
+//
+//  Created by devannogiovanni on 21/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iChooseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
