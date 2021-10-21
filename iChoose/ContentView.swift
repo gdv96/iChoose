cd1d4ef18614b94b92af7d33bc401767da128d52
Giovanni <giovannidv96@gmail.com>
2021-10-21 19:25:38 +0200

    Modifiche

--- iChoose/ContentView.swift
+++ iChoose/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var firstPage: Bool = true
+    @State var firstPage1: Bool = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if (firstPage){
+                TabView {
+                    welcomeUIView()
+                    //page 1
+                    readNewsUIView()
+                    //page 2
+                    ReportsUIView()
+                    //page 3
+                    VotingUIView(isFirst: $firstPage)
+                    //page 4
+                }
+                .tabViewStyle(.page(indexDisplayMode: .always))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+        } else if (firstPage1){
+            
+            BenvenutoUIView(isFirst1: $firstPage1)
+            
+        } else {
+            TabUIView()
+                
+        }
     }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
