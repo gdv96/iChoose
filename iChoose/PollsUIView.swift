c40a44e88d58d65d9d38184c153a158e85870627
Giovanni <giovannidv96@gmail.com>
2021-10-21 19:02:35 +0200

    Creazione PollsUIView

---
+++ iChoose/PollsUIView.swift
@@ -0,0 +1,147 @@
+//
+//  PollsUIView.swift
+//  iChoose
+//
+//  Created by devannogiovanni on 21/10/21.
+//
+
+import SwiftUI
+struct Poll: Identifiable{
+    var titlePoll: String
+    var imageNamePoll: String
+    var textPoll: String
+    var remainingTime: String
+    var progressBar: Float
+    var numberVotes: Int
+    var typePoll: Int
+    var id = UUID()
+}
+
+struct PollsUIView: View {
+       @State private var speed = 50.0
+       @State private var isEditing = false
+       @State private var pollsState = 0
+       @State private var searchText = ""
+       @ObservedObject private var appPoll = PollMemory.shared
+       @ObservedObject private var appSetting = Settings.shared
+    
+    var body: some View {
+        
+        NavigationView{
+            VStack {
+                           Picker("", selection: $pollsState) {
+                               Text("Ongoing").tag(0)
+                               Text("Expired").tag(1)
+                               Text("Voted").tag(2)
+                           }
+                           .pickerStyle(.segmented)
+                           .padding(.horizontal)
+                       if pollsState == 0 {
+                           if((appSetting.username.isEmpty) && (appSetting.password.isEmpty)) || (appSetting.logOut.isEmpty) {
+                               ScrollView(.vertical, showsIndicators: false){
+                                   VStack{
+                                      Group{
+                                          ForEach(searchResults) {pollOngoing in
+                                              NavigationLink(destination: OngoingDetailNotUserUIView(poll: pollOngoing)) {
+                                                   CardViewOngoingUIView(pollsOngoing: pollOngoing)
+                                                }
+                                               }
+                                           }
+                                       }.frame(width: 350)
+                                        .minimumScaleFactor(0.5)
+                                   }
+                           }else{
+                               ScrollView(.vertical, showsIndicators: false){
+                                   VStack{
+                                      Group{
+                                          ForEach(searchResults) {pollOngoing in
+                                              NavigationLink(destination:OngoingDetailUserUIView(poll: pollOngoing)) {
+                                                   CardViewOngoingUIView(pollsOngoing: pollOngoing)
+                                                }
+                                               }
+                                           }
+                                       }.frame(width: 350)
+                                        .minimumScaleFactor(0.5)
+                                   }
+                           }
+                           
+                       }
+                           else if pollsState == 1 {
+                               ScrollView(.vertical, showsIndicators: false){
+                                   VStack{
+                                      Group{
+                                          ForEach(searchResults2) {pollExpired in
+                                              NavigationLink(destination:ExpiredDetailUIView(poll: pollExpired)) {
+                                                  CardViewExpiredUIView(pollsExpired: pollExpired)
+                                                }
+                                               }
+                                           }
+                                       }.frame(width: 350)
+                                        .minimumScaleFactor(0.5)
+                                   }
+                           } else {
+                               ScrollView(.vertical, showsIndicators: false){
+                                   VStack{
+                                      Group{
+                                          ForEach(searchResults3) {pollVoted in
+                                              NavigationLink(destination:VotedDetailUIView(poll: pollVoted)) {
+                                                  CardViewVotedUIView(pollsVoted: pollVoted)
+                                                }
+                                              
+                                               }
+                                           }
+                                       }.frame(width: 350)
+                                        .minimumScaleFactor(0.5)
+                                   }
+                           }
+
+                           Spacer()
+    }.navigationBarTitle("Polls", displayMode:.automatic)
+        }//.navigationBarTitle("Polls", displayMode:.automatic)
+            .searchable(text: $searchText)
+                       }
+    
+    var searchResults: [Poll]{
+        if searchText.isEmpty{
+            return appPoll.getOngoing()
+        } else {
+            return appPoll.getOngoing().filter{(pollOngoing: Poll) in
+                return pollOngoing.titlePoll.localizedCaseInsensitiveContains(searchText)
+                
+        }
+    }
+        
+   }
+
+    var searchResults2: [Poll]{
+        if searchText.isEmpty{
+            return appPoll.getExpired()
+        } else {
+            return appPoll.getExpired().filter{(pollExpired: Poll) in
+                return pollExpired.titlePoll.localizedCaseInsensitiveContains(searchText)
+                
+        }
+    }
+        
+   }
+    
+    var searchResults3: [Poll]{
+        if searchText.isEmpty{
+            return appPoll.getVoted()
+        } else {
+            return appPoll.getVoted().filter{(pollVoted: Poll) in
+                return pollVoted.titlePoll.localizedCaseInsensitiveContains(searchText)
+                
+        }
+    }
+        
+   }
+    
+    
+}
+
+struct PollsUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        PollsUIView()
+    }
+}
